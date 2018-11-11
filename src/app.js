757391c38751737e12dc6f1d1c89d574919dcfc5
gm <support@feine-muehle.at>
2018-11-11 13:18:10 +0100

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -1,25 +1,34 @@
-// components are split into files, easier to maintain
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-// 1 setup provider for components in the application
 import { Provider } from 'react-redux';
-//import myApp from './components/myapp';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+
+
 
 const store = configureStore();
 
-//store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
-//store.dispatch(addExpense({ description: 'Gass Bill', amount: 3000  }));
-//store.dispatch(addExpense({ description: 'Rent', amount: 8764578 }));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
 
 
+// console.log('testing dev environment');
+// console.log(store.getState());
+// store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
+// store.dispatch(addExpense({ description: 'Gass Bill', amount: 3000  }));
+// store.dispatch(addExpense({ description: 'Rent', amount: 8764578 }));
 
 // store.dispatch(setTextFilter(''));
 
@@ -27,21 +36,13 @@ const store = configureStore();
 //     store.dispatch(setTextFilter('water'));
 // }, 3000)
 
-
-
-
 // const state = store.getState();
 // const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(store.getState());
-
 // provider give components access to the store
 // 2 this code will not be changed
-const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-);
 
 // render with passed options
-ReactDOM.render(jsx, document.getElementById('app'));
+
+// components are split into files, easier to maintain
+// 1 setup provider for components in the application
