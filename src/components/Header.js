6ce22d516fc18e3dc2aaa26510ce686c5a91a196
gm <support@feine-muehle.at>
2018-11-15 11:35:12 +0100

    15 nov 1134

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
     <header>
-        <h1>Header Expensify App</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>HOME</NavLink>----
-        <NavLink to="/create" activeClassName="is-active">ADD EXPENSE</NavLink>----
-        <NavLink to="/help" activeClassName="is-active">HELP</NavLink>----
+        <h1>Header Expensify App II</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>HOME</NavLink>
+        <NavLink to="/create" activeClassName="is-active">ADD EXPENSE</NavLink>
+        <NavLink to="/help" activeClassName="is-active">HELP</NavLink>
     </header>
 );
 
